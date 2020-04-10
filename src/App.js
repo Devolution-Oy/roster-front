784332f02ccdb95729d0048793e4181aa456dd6d
Mikko Kurkela <mikko.kurkela_c@sandvik.com>
2020-04-10 18:20:23 +0300

    Testing eslint pre-commit hook

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
-  Route }
-from 'react-router-dom';
+  Route
+} from 'react-router-dom';
 
 import FrontPage from './pages/Front';
 import AuthPage from './pages/Auth';
